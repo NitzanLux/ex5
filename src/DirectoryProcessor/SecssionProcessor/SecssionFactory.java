3d75c73d7e0bcff944b4d215330ca216d2794fa8
goolux <nitzanlux@gmail.com>
2018-06-03 19:44:45 +0300

    secssionFactory

---
+++ src/DirectoryProcessor/SecssionProcessor/SecssionFactory.java
@@ -0,0 +1,15 @@
+package DirectoryProcessor.SecssionProcessor;
+
+public class SecssionFactory {
+    private static SecssionFactory instance=new SecssionFactory();
+    private SecssionFactory(){}
+
+    public static SecssionFactory getInstance() {
+        return instance;
+    }
+    public Secssion creatSecssion(String[] secssionStrings){
+        
+
+    }
+
+}
