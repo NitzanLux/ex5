3f11b7c63b525d99145317e9befd3239940b4807
liorait <liorali22@gmail.com>
2018-06-08 00:20:52 +0300

    errors

--- src/filesprocessing/TypeTwoExceptions.java
+++ src/filesprocessing/TypeTwoExceptions.java
@@ -3,7 +3,6 @@ package filesprocessing;
 /**
  * The class represents
  */
-public abstract class TypeTwoExceptions extends Exception {
 abstract class TypeTwoExceptions extends Exception {
     private static final long serialVersionUID=1L;
     private static final String TYPE_2_ERROR_PREFIX = "ERROR: %s\n";
