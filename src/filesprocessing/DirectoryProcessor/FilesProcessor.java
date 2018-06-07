2c1f4ea9785ae26a5a8653515426e1918a9f0345
goolux <nitzanlux@gmail.com>
2018-06-07 23:49:37 +0300

    CurrentSecssion needed to be updateff

--- src/filesprocessing/DirectoryProcessor/FilesProcessor.java
+++ src/filesprocessing/DirectoryProcessor/FilesProcessor.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  */
 public class FilesProcessor {
 
-    static final int MIN_NUM_OF_ARGUMENTS = 1;
+    private static final int MIN_NUM_OF_ARGUMENTS = 1;
     private static final int PATH_NAME_POSITION = 0;
     private static final int COMMAND_NAME_POSITION = 1;
 
