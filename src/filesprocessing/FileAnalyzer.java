d073fea94d028947625a09bfd566f2583e68319a
liora <liorali22@gmail.com>
2018-06-08 11:35:23 +0300

    תיעוד

--- src/filesprocessing/FileAnalyzer.java
+++ src/filesprocessing/FileAnalyzer.java
@@ -1,7 +1,5 @@
 package filesprocessing;
 
-
-
 import filesprocessing.secssionsprocessor.CurrentSecession;
 import filesprocessing.secssionsprocessor.SecessionCreationException;
 
@@ -9,7 +7,7 @@ import java.util.ArrayList;
 import java.lang.*;
 
 /**
- *
+ *  Represents an instance of FileAnalyzer object that is used to analyze the data in the ArrayList of strings
  */
 class FileAnalyzer {
     private static FileAnalyzer instance=new FileAnalyzer();
@@ -33,29 +31,32 @@ class FileAnalyzer {
         return instance;
     }
 
-    /* This method goes through the array list that contains the file data, First,
+    /*This method goes through the array list that contains the file data. First,
     * it looks for type 2 exceptions, if found,
     * it throws exceptions. If there are no type 2 exceptions, it goes over the file, and for each section
-    *it filters and orders the files
+    * it filters and orders the files
     */
     void analyzeStringList(ArrayList<String> fileData) throws TypeTwoExceptions.BadFilterSectionName,
             TypeTwoExceptions.BadOrderSectionName, TypeTwoExceptions.BadFormatFile {
         checkTypeTwoErrors(fileData); // Checks if there are type 2 errors in the file
-        String filterValue ;
-        String orderValue ;
+        String filterValue;
+        String orderValue;
         int filterLine=0;
         int orderLine=0;
         // This loop goes over the array list and filters and sorters files by each section
         for (int i = 0; i < fileData.size();) {
             i++;
+            // Checks if we reached the last index in fileData, in that case, stops the iteration over fileDate
             if (i>=fileData.size()){
                 break;
             }
             filterValue=fileData.get(i);
             i+= JUMP_TO_ORDER;
+            // Checks if we reached the last index in fileData, in that case orderValue gets a default value
             if (i>=fileData.size()){
                 orderValue=DEFAULT_SORTER;
             }else {
+                // Checks if the current line is FILTER headline
                 if (fileData.get(i).equals(FILTER_HEADLINE)){
                     orderValue=DEFAULT_SORTER;
                 }else {
@@ -75,14 +76,15 @@ class FileAnalyzer {
         }
     }
 
-    // private method to print the names of the filtered and sorted files
+    /* private method to print the names of the filtered and sorted files */
     private void printFiles(String[] filesToPrint){
+        // This loop iterates through the array of filesToPrint and prints the name of each file
         for (int i = 0; i< filesToPrint.length; i++){
             System.out.println(filesToPrint[i]);
         }
     }
 
-
+    /* private method to check type 2 errors */
      private void checkTypeTwoErrors(ArrayList<String> fileData) throws TypeTwoExceptions.BadFilterSectionName,
              TypeTwoExceptions.BadOrderSectionName, TypeTwoExceptions.BadFormatFile {
             // This loop iterates through the array, for each section checks if in the section exists type 2 errors
@@ -92,14 +94,13 @@ class FileAnalyzer {
             }
      } // end of checkTypeTwoErrors method
 
-
-
-
+    /* private method that checks if there are type 2 errors in the FILTER section */
     private int checkFilter(ArrayList<String> fileData, int lineNumber) throws TypeTwoExceptions.BadFilterSectionName,
             TypeTwoExceptions.BadOrderSectionName, TypeTwoExceptions.BadFormatFile {
         boolean isFilterHeadLine=false;
         boolean isFilterValue=false;
         for (int i = 0; i <3; i++) {
+            // Checks if the current line is FILTER headline
             if(fileData.get(lineNumber).equals(FILTER_HEADLINE) && !isFilterHeadLine){
                 isFilterHeadLine=true;
                 lineNumber++;
@@ -110,7 +111,7 @@ class FileAnalyzer {
             }else if (isFilterHeadLine&&isFilterValue&&fileData.get(lineNumber).equals(ORDER)){
                 return lineNumber;
             }
-        }//if the
+        }
         switch (fileData.get(lineNumber).toUpperCase()) {
             case ORDER:
                 throw new TypeTwoExceptions.BadFilterSectionName();
@@ -121,24 +122,27 @@ class FileAnalyzer {
         }
     } // end of checkFilter method
 
+    /* private method that checks if there are type 2 errors in the ORDER section */
     private int checkOrder(ArrayList<String> fileData, int lineNumber) throws TypeTwoExceptions.BadOrderSectionName,
             TypeTwoExceptions.BadFilterSectionName, TypeTwoExceptions.BadFormatFile {
 
-        boolean isSortHeadLine=false;
-        boolean isSortValue=false;
-        for (int i = 0; i <3; i++) {
-            if (lineNumber>=fileData.size()){
-                return fileData.size()-1;
+        boolean isSortHeadLine = false;
+        boolean isSortValue = false;
+        for (int i = 0; i < 3; i++) {
+            // Checks if we reached the end of fileData
+            if (lineNumber >= fileData.size()) {
+                return fileData.size() - 1;
             }
-            if (!isSortHeadLine&&fileData.get(lineNumber).equals(ORDER)){
-                isSortHeadLine=true;
+            // Checks if the current line is ORDER headline
+            if (!isSortHeadLine && fileData.get(lineNumber).equals(ORDER)) {
+                isSortHeadLine = true;
                 lineNumber++;
-            }else if (!fileData.get(lineNumber).equals(ORDER)){
-                if (fileData.get(lineNumber).equals(FILTER_HEADLINE)){
+            } else if (!fileData.get(lineNumber).equals(ORDER)) {
+                if (fileData.get(lineNumber).equals(FILTER_HEADLINE)) {
                     return lineNumber;
-                }else if (!isSortValue){
+                } else if (!isSortValue) {
                     lineNumber++;
-                    isSortValue=true;
+                    isSortValue = true;
                 }
             }
         }
@@ -151,5 +155,5 @@ class FileAnalyzer {
             default:
                 throw new TypeTwoExceptions.BadFormatFile();
         }
-        }
+    }
 }
